c6b558fd779cab6f6ee928f68698cf2b16fe229e
js1804 <mraarabzade1377@gmail.com>
2019-12-14 22:27:58 +0330

    it had a Seriouse problem in random forest makking and i fixed it and output are appended

--- main.py
+++ main.py
@@ -35,18 +35,28 @@ def bagging(feachers = NONE_TARGET_FEACHER):
 def random_forrest():
     l = []
     rand = make_random_list()
-    random_feacher_ind = np.random.randint(low = 0, high=len(NONE_TARGET_FEACHER), size = 5)
-    random_feacher = []
+    random_feacher = [[],[],[],[],[]]
     for i in range(5):
-        random_feacher.append(NONE_TARGET_FEACHER[random_feacher_ind[i]])
-    for i in range(5):
+        for j in range(5):
+            random_feacher_ind = np.random.randint(low = 0, high=len(NONE_TARGET_FEACHER), size = 5)
+            random_feacher[i].append(NONE_TARGET_FEACHER[random_feacher_ind[i]])
         l.append(tree.DecisionTreeClassifier(max_features = 5))
-        l[i] = l[i].fit(data[random_feacher].iloc[rand[i]], data['target'].iloc[rand[i]])
+        l[i] = l[i].fit(data[random_feacher[i]].iloc[rand[i]], data['target'].iloc[rand[i]])
+        random_feacher_ind = np.random.randint(low = 0, high=len(NONE_TARGET_FEACHER), size = 5)
     return l, random_feacher
 def eval_ensemmble(bagging_trees, feachers = NONE_TARGET_FEACHER):
     answers = []
     for i in range(5):
         answers.append(bagging_trees[i].predict(data[feachers][MAX_RANGE:]))
+    for i in range(4):
+        for j in range(len(answers[0])):
+            answers[0][j] += answers[i+1][j]
+    answers[0] = list(map(lambda x: 1 if x > 2 else 0, answers[0]))
+    return calc_acc(answers[0])
+def eval_random_forest(bagging_trees, feachers):
+    answers = []
+    for i in range(5):
+        answers.append(bagging_trees[i].predict(data[feachers[i]][MAX_RANGE:]))
     for i in range(4):
         for j in range(len(answers[0])):
             answers[0][j] += answers[i+1][j]
@@ -60,13 +70,13 @@ def find_important_feacher():
         if res <45 :
             print(NONE_TARGET_FEACHER[i], '\t -->', res)
 bagging_trees = bagging()
-random_forrest, feachers_selected = random_forrest()
+random_forrests, feachers_selected = random_forrest()
 print(feachers_selected)
 print(eval_ensemmble(bagging_trees))
-print(eval_ensemmble(random_forrest, feachers=feachers_selected))
+print(eval_random_forest(random_forrests, feachers_selected))
 print(decision_tree())
-for i in range(100):
-    print('--------new test------------')
-    find_important_feacher()
-    print('\n\n')
+# for i in range(100):
+#     print('--------new test------------')
+#     find_important_feacher()
+#     print('\n\n')
 # print(decision_tree())
